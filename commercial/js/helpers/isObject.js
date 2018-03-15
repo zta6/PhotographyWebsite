fee9eb53874bfce962bf60c8578dd536b4b570c4
Zachary Adam <zta6@pitt.edu>
2018-03-14 21:19:18 -0400

    delete landing page and commercial page

--- commercial/js/helpers/isObject.js
+++
@@ -1,7 +0,0 @@
-/**
- * Checks if item is an object
- * @param obj {Mixed}
- */
-const isObject = obj => obj === Object(obj) && Object.prototype.toString.call(obj) !== '[object Array]';
-
-export default isObject;
